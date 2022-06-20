73fb49ee48ce3c20006791c2b6e88cc0e8828358
jomare1188 <jomare1188@gmail.com>
2022-06-20 15:02:53 -0300

    Update pratica_rnaseq.sh

--- pratica_rnaseq.sh
+++ pratica_rnaseq.sh
@@ -1,3 +1,12 @@
+
+mkdir -p PRATICA_RNASEQ
+cd PRATICA_RNASEQ
+scp -P 1000 jorge.munoz@bioinfo.cena.usp.br:/Storage/data1/jorge.munoz/PRACTICA_RNASEQ/data/tiny/data.zip
+unzip data.zip
+
+
+
+
 # go wd
 cd ~/PRACTICA_RNASEQ
 mkdir -p prefetch
