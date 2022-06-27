3cb5ef3d6a48ab49361705dfe1431fe071b5f400
jomare1188 <jomare1188@gmail.com>
2022-06-27 14:16:11 -0300

    Update pratica_rnaseq.sh

--- pratica_rnaseq.sh
+++ pratica_rnaseq.sh
@@ -8,9 +8,27 @@ unzip ./../data.zip
 cd ..
 rm data.zip
 
+
+apt update -y && apt upgrade -y
 # Install r
-conda create -n R4.2 r-base=4.2
-rstudio
+# update indices
+apt update -qq
+# install two helper packages we need
+apt install --no-install-recommends software-properties-common dirmngr
+# add the signing key (by Michael Rutter) for these repos
+# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
+# Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt install --no-install-recommends r-base
+
+
+
+
+
+
+#######
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("DESeq2")
